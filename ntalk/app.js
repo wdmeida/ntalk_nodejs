dc41be251dc5944a58e5a75272f5cc75260bf75d
wdmeida <wdmeida@gmail.com>
2016-04-25 23:32:00 -0300

    Atualização de projeto

--- ntalk/app.js
+++ ntalk/app.js
@@ -4,6 +4,10 @@ var express = require('express'),
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 //app.get('/', routes.index);
--- ntalk/routes/home.js
+++ ntalk/routes/home.js
@@ -6,7 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//Define as rotas que serão utilizadas pela aplicação.
 module.exports = function(app) {
   var home = app.controllers.home;
   app.get('/', home.index);
+  app.post('/entrar', home.login);
+  app.get('/sair', home.logout);
 };
