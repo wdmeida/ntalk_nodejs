12e1113974b9b994bee94d64aaf12075e1911583
wdmeida <wdmeida@gmail.com>
2016-07-01 11:35:10 -0300

    Refatoração da conexão do cliente com o servidor.

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,7 +1,9 @@
 var express = require('express')
     , app = express()
     , load = require('express-load')
-    , error = require('./middleware/error');
+    , error = require('./middleware/error')
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -9,16 +11,6 @@ app.use(express.cookieParser('ntalk'));
 app.use(express.session());
 app.use(express.json());
 app.use(express.urlencoded());
-/*
-  express.methodOverride() permite utilizar um mesmo path entre os
-  métodos do HTTp, fazendo uma sobrestrita de métodos.
-*/
-app.use(express.methodOverride());
-/*
-  Gerencia as rotas da aplicação, permitindo a implementação de rotas
-  para páginas de erros e rotas para arquivos estáticos, sem conflitar
-  com as rotas da aplicação.
-*/
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 app.use(error.notFound);
@@ -27,6 +19,14 @@ app.use(error.serverError);
 //app.get('/usuarios', routes.user.index);
 
 load('models').then('controllers').then('routes').into(app);
+
+io.sockets.on('connection', function (client) {
+  client.on('send-server', function (data){
+    var msg = "<b>" + data.nome + ":</b> " + data.msg + "<br>";
+    client.emit('send-client', msg);
+    client.broadcast.emit('send-client', msg);
+  });
+});
 
 app.listen(3000, function(){
   console.log("Ntalk no ar...");
