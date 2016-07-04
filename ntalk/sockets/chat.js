e11475043286e93268d3ef4ceb8228d6cee13b8e
wdmeida <wdmeida@gmail.com>
2016-07-04 18:50:54 -0300

    Configuração do histórico de mensagens armazenadas com Redis.

--- ntalk/sockets/chat.js
+++ ntalk/sockets/chat.js
@@ -1,5 +1,7 @@
 module.exports = function (io) {
+
   var crypto = require('crypto')
+    , redis = require('redis').createClient()
     , md5 = crypto.createHash('md5')
     , sockets = io.sockets;
 
@@ -27,11 +29,30 @@ module.exports = function (io) {
       }
       client.set('sala', sala);
       client.join(sala);
+
+      var msg = "<b>" + usuario.nome + ":</b> entrou.<br>";
+
+      //redis.lpush(sala, msg) adiciona na lista de mensagem.
+      redis.lpush(sala, msg, function(erro, res) {
+
+        /*
+          redis.lrange(sala, 0, -1) retorna um array contendo os elementos a partir de
+           um range inicial e final da lista. O range utiliza dois índices, e neste Caso
+           o índice inciial é 0 e o final é -1. Quando informamos o valor -1 no índice final
+           indicamos que o range será total, retornando todos os elementos da lista.
+        */
+        redis.lrange(sala, 0, -1, function(erro, msgs) {
+          msgs.forEach(function(msg) {
+            sockets.in(sala).emit('send-client', msg);
+          });
+        });
+      });
     });
 
     client.on('send-server', function (data) {
       var msg = "<b>" + usuario.nome + ":</b> " + msg + "<br>";
       client.get('sala', function(erro, sala) {
+        redis.lpush(sala, msg);
         var data = {email: usuario.email, sala: sala};
         cliente.broadcast.emit('new-message', data);
         sockets.in(sala).emit('send-client', msg);
@@ -41,6 +62,7 @@ module.exports = function (io) {
     client.on('disconnect', function () {
       client.get('sala', function(erro, sala) {
         var msg = "<b>" + usuario.nome + ":</b> saiu.<br>";
+        redis.lpush(sala, msg);
         client.broadcast.emit('notify-offlines', usuario.email);
         sockets.in(sala).emit('send-client', msg);
         client.leave(sala);
