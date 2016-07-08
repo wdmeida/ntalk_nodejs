930b6b4e6847361c83d7f887036977f8464413fe
wdmeida <wdmeida@gmail.com>
2016-07-08 19:08:28 -0300

    Correção do app.js

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,67 +1,74 @@
 var express = require('express')
-    , app = express()
-    , load = require('express-load')
-    , server = require('http').createServer(app)
-    , error = require('./middleware/error')
-    , cfg = require('./config.json')
-    , io = require('socket.io').listen(server)
-    , mongoose = require('mongoose')
-    , redis = require('./libs/redis_connect')
-    , ExpressStore = redis.getExpressStore()
-    , SocketStore = redis.getSocketStore();
+  , cfg = require('./config.json')
+  , load = require('express-load')
+  , bodyParser = require('body-parser')
+  , cookieParser = require('cookie-parser')
+  , expressSession = require('express-session')
+  , methodOverride = require('method-override')
+  , error = require('./middleware/error')
+  , compression = require('compression')
+  , csurf = require('csurf')
+  , redisAdapter = require('socket.io-redis')
+  , RedisStore = require('connect-redis')(expressSession)
+  , app = express()
+  , server = require('http').Server(app)
+  , io = require('socket.io').listen(server)
+  , cookie = cookieParser(cfg.SECRET)
+  , store = new RedisStore({prefix: cfg.KEY});
 
-//Configurações de session e cookies
-var cookie = express.cookieParser(cfg.SECRET)
-  , storeOpts = {client: redis.getClient(),
-                 prefix: cfg.KEY}
-  , store = new ExpressStore(storeOpts)
-  , sessOpts = {secret: cfg.SECRET,
-                key: cfg.KEY,
-                store: store}
-  , session = express.session(sessOpts);
-
-//Stack de configurações do Express.
-app.use(express.logger('dev'));
+//Stack de configuração do Express.
+app.disable('x-powered-by');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(compression());
 app.use(cookie);
-app.use(session);
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.compress(cfg.GZIP_LVL))
-app.use(app.router);
-app.use(express.static(__dirname + '/public', cfg.MAX_AGE));
-app.use(error.notFound);
-app.use(error.serverError);
+app.use(expressSession({
+  secret: cfg.SECRET,
+  name: cfg.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: store
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public', cfg.CACHE));
+app.use(csurf());
+app.use(function(req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
-//Stack de configurações do Socket.IO
-io.enable('browser client cache');
-io.enable('browser client minification');
-io.enable('browser client etag');
-io.enable('browser client gzip');
-io.set('log level', 1);
-io.set('store', new SocketStore);
-io.set('authorization', function(data, accept) {
+//Stack de configuração do socket.io
+io.adapter(redisAdapter(cfg.REDIS));
+io.use(function(socket, next) {
+  var data = socket.request;
   cookie(data, {}, function(err) {
     var sessionID = data.signedCookies[cfg.KEY];
     store.get(sessionID, function(err, session) {
       if (err || !session) {
-        accept(null, false);
+        return next(new Error('not authorized'));
       } else {
-        data.session = session;
-        accept(null, true);
+        socket.handshake.session = session;
+        return next();
       }
     });
   });
 });
 
-load('models').then('controllers').then('routes').into(app);
-load('sockets').into(io);
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app)
+;
+load('sockets')
+  .into(io);
+
+app.use(error.notFound);
+app.use(error.serverError);
 
 server.listen(3000, function(){
-  console.log("Ntalk no ar...");
+  console.log("Ntalk no ar.");
 });
 
-//Exporta a variável que contém as funções do servidor da aplicação.
 module.exports = app;
